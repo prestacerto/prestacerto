fc24b20ccb4f9757ab24d28f494b5be047d8f339
prestacerto <prestacerto@gmail.com>
2026-08-18 10:18:24 -0300

    feat: rota real para publicação de projetos

---
+++ src/app/(protected)/dashboard/projects/new/page.tsx
@@ -0,0 +1,7 @@
+import { PublishProjectForm } from "@/components/projects/publish-project-form";
+
+export const dynamic = "force-dynamic";
+
+export default function NewProjectPage() {
+  return <PublishProjectForm />;
+}
